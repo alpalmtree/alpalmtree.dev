4fd13ce31942f23ab8823e168fc3df2fbd4b679d
Al <alvaro.palma@prontonmail.com>
2025-07-09 14:44:44 +0200

    add about me info

--- blog/web-components-miedo-y-asco-en-la-web/metadata.js
+++ blog/web-components-miedo-y-asco-en-la-web/metadata.js
@@ -4,10 +4,10 @@ export default {
     pubDate: new Date("2025-06-14"),
     author: "Álvaro Palma",
     image: {
-        url: "web-components-otro-punto-de-vista",
+        url: "web-components-miedo-y-asco-en-la-web",
         alt: "Imagen en blanco y negro con dithering de una pieza de lego insertada en medio de una placa."
     },
     tags: ["web-components", "javascript"],
     description: "Causantes de decepción en algunas personas e ilusión de un mundo sin React en otras. ¿Y si cambiamos la perspectiva un poquito?" ,
-    slug: "web-components-otro-punto-de-vista",
+    slug: "web-components-miedo-y-asco-en-la-web",
 }
