459720e0c695dd714f9df9e9b608c3868a35ba67
Al <alvaro.palma@prontonmail.com>
2025-08-03 10:16:13 +0200

    update este blog estaba hecho con astro

--- blog/este-blog-estaba-hecho-con-astro/metadata.js
+++ blog/este-blog-estaba-hecho-con-astro/metadata.js
@@ -2,7 +2,7 @@
 export default {
     title: "Este blog estaba hecho con Astro",
     pubDate: new Date("2025-07-25"),
-    updatedDate: null,
+    updatedDate: new Date("2025-08-03"),
     author: "Álvaro Palma",
     tags: ["opinión", "javascript"],
     description: "Pero me dije a mí mismo: ¿Dónde está la gracia? Así que decidí migrarlo a mi propio SSG hecho desde 0, en menos de 100 líneas de JS.",
